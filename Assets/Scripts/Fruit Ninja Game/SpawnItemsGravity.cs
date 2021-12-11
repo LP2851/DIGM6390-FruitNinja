af0014672e86f518a67cdfa5a6dde5496109e8bd
kai Bergman <kaibergman123@gmail.com>
2021-12-11 18:30:43 +0000

    added 10 varients of fruit to spawner - some scaling issues

--- Assets/Scripts/Fruit Ninja Game/SpawnItemsGravity.cs
+++ Assets/Scripts/Fruit Ninja Game/SpawnItemsGravity.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Diagnostics;
 
 public class SpawnItemsGravity : MonoBehaviour
 {
     public float spawnTime=1; //Spawn Time
-    public GameObject apple, bomb;
+    public GameObject apple, bomb, orange, bananas, blueApple, grapes, lemon, peach, pear, pinapple, watermelon;
     public float upForce = 750; //Up force
     public float leftRightForce = 200; //Left and right force
     public float maxX = -7; //Max x spawn position
@@ -13,7 +14,7 @@ public class SpawnItemsGravity : MonoBehaviour
 
     public float sizeDecreaseStep = 0.05f;
     public float minSize = 0.5f;
-
+    
     void Start()
     {
         //Start the spawn update
@@ -21,6 +22,9 @@ public class SpawnItemsGravity : MonoBehaviour
     }
     IEnumerator Spawn()
     {
+        //pick a random fruit
+        int RandomOption;
+        RandomOption = Random.Range(1, 12);
         //Wait spawnTime
         yield return new WaitForSeconds(spawnTime);
         //Spawn prefab is apple
@@ -33,6 +37,69 @@ public class SpawnItemsGravity : MonoBehaviour
             prefab = bomb;
 
         }
+        //changes the prefab to a random fruit
+        switch (RandomOption)
+            {
+                case 1:
+                    prefab = bomb;
+                    System.Diagnostics.Debug.WriteLine("bomb");
+                    break;
+
+                case 2:
+                    prefab = apple;
+                    System.Diagnostics.Debug.WriteLine("apple");
+                    break;
+
+                case 3:
+                    prefab = orange;
+                    System.Diagnostics.Debug.WriteLine("orange");
+                    break;
+
+                case 4:
+                    prefab = bananas;
+                    System.Diagnostics.Debug.WriteLine("bananas");
+                    break;
+
+                case 5:
+                    prefab = blueApple;
+                    System.Diagnostics.Debug.WriteLine("blueapple");
+                    break;
+
+                case 6:
+                    prefab = grapes;
+                    System.Diagnostics.Debug.WriteLine("grapes");
+                    break;
+
+                case 7:
+                    prefab = lemon;
+                    System.Diagnostics.Debug.WriteLine("lemon");
+                    break;
+
+                case 8:
+                    prefab = peach;
+                    System.Diagnostics.Debug.WriteLine("peach");
+                    break;
+
+                case 9:
+                    prefab = pear;
+                    System.Diagnostics.Debug.WriteLine("pear");
+                    break;
+                case 10:
+                    prefab = pinapple;
+                    System.Diagnostics.Debug.WriteLine("pineapple");
+                    break;
+
+                case 11:
+                    prefab = watermelon;
+                    System.Diagnostics.Debug.WriteLine("watermelon");
+                    break;
+
+                default:
+                    System.Diagnostics.Debug.WriteLine("nothing");
+                    break;
+            }
+
+
         //Spawn prefab add random position
         GameObject go = Instantiate(prefab,new Vector3(Random.Range(minX
             ,maxX + 1),transform.position.y, 3f),Quaternion.Euler(0,0, Random.Range (-
